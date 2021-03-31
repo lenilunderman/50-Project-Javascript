de0996a69f026110236389a7922a5981a37bb03b
lenilunderman <lenilunderman@gmail.com>
2021-03-31 18:18:08 -0400

    Expandable Cards

--- 01 -Expandable Cards/script.js
+++ 01 -Expandable Cards/script.js
@@ -1,1 +1,18 @@
-console.log('Hi, I am in the javascript')
+// bring all the panels to change the classe to active
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    // function to remove the classe before injecting a new one
+    removeActiviteClasses()
+    panel.classList.add('actvite')
+  })
+})
+
+function removeActiviteClasses() {
+  // loop all the panels
+  panels.forEach((panel) => {
+    panel.classList.remove('actvite')
+  })
+}
